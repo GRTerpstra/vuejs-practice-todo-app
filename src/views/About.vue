cf4745f21b158240cb07f374dbab48357999578a
GRTerpstra <gerwin_terpstra@live.nl>
2022-02-11 14:42:50 +0100

    First commit.

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-16">
+    <h1>About Vuetify Todo</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam dignissimos quo voluptatibus culpa voluptates at dolore laborum sit, assumenda repudiandae alias, inventore sunt sequi dicta aut debitis velit ea porro.</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam dignissimos quo voluptatibus culpa voluptates at dolore laborum sit, assumenda repudiandae alias, inventore sunt sequi dicta aut debitis velit ea porro.</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam dignissimos quo voluptatibus culpa voluptates at dolore laborum sit, assumenda repudiandae alias, inventore sunt sequi dicta aut debitis velit ea porro.</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam dignissimos quo voluptatibus culpa voluptates at dolore laborum sit, assumenda repudiandae alias, inventore sunt sequi dicta aut debitis velit ea porro.</p>
   </div>
 </template>
